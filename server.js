f27c6d7e96e7c894f9e7683840b5d324c4ea5320
ramontallion01 <52453990+ramontallion01@users.noreply.github.com>
2019-07-12 19:19:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
-const server = app.listen(7000, () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
+// ...
+app.set('view engine', 'pug');
+
+// serve static files from the `public` folder
+app.use(express.static(__dirname + '/public'));
+// ...
